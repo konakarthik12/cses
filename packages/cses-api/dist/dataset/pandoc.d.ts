87449a46a24de73c01ad09ad1fdc0f25617036d7
Karthikeya Kona <konakarthik12@gmail.com>
2021-07-24 13:15:58 -0400

    changes

--- packages/cses-api/dist/dataset/pandoc.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function htmlToText(input: string): Promise<string>;
--- packages/cses-cli/dist/utils/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-function sleep(time) {
-    return new Promise(res => setTimeout(res, time));
-}
-exports.sleep = sleep;
